7c16f84cbb1d36cf9cea1eef0bede3e7721c32ec
koopass <kukupa86@gmail.com>
2023-07-23 12:34:07 +0800

    feat: orm

--- orm/mysql.go
+++ orm/mysql.go
@@ -17,6 +17,7 @@ type EasyCRUD[T Entity] interface {
 	SelectOne(ctx context.Context, query interface{}, args ...interface{}) (T, error)
 	Select(ctx context.Context, query interface{}, args ...interface{}) ([]T, error)
 	SelectLimit(ctx context.Context, limit int, query interface{}, args ...interface{}) ([]T, error)
+	SelectPage(ctx context.Context, offset, limit int, query interface{}, args ...interface{}) ([]T, error)
 	InsertOne(ctx context.Context, entity T, cols []string) (int64, error)
 	UpdateOne(ctx context.Context, entity T, cols []string) (int64, error)
 	DeleteOne(ctx context.Context, id int64) error
@@ -35,6 +36,18 @@ type EasyGORM[T Entity] struct {
 func (e *EasyGORM[T]) SelectLimit(ctx context.Context, limit int, query interface{}, args ...interface{}) ([]T, error) {
 	var rows []T
 	err := e.db.Where(query, args...).Limit(limit).Find(&rows).Error
+	if err != nil {
+		return nil, err
+	}
+	if len(rows) == 0 {
+		return nil, nil
+	}
+	return rows, nil
+}
+
+func (e *EasyGORM[T]) SelectPage(ctx context.Context, offset, limit int, query interface{}, args ...interface{}) ([]T, error) {
+	var rows []T
+	err := e.db.Where(query, args...).Offset(offset).Limit(limit).Find(&rows).Error
 	if err != nil {
 		return nil, err
 	}
